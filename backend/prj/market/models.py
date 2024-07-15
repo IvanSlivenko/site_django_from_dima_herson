6ff66975c422ed4529bd50f2a821c9e52f689d5b
IvanSlivenko <ivan8822@ukr.net>
2024-07-15 17:24:59 +0300

    15-07-2024-17-24

---
+++ backend/prj/market/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Provider(User):
+    name = models.CharField(max_length=250, default='')
+    phone = models.CharField(max_length=250, default='')
+    rating = models.IntegerField(default=0)
+
